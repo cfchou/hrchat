97429dbca684ac62d02d042384415bd62bc550d7
cfchou <root@wasabi.(none)>
2013-03-02 11:05:35 +0000

    to connect

--- HRChat.hs
+++ HRChat.hs
@@ -3,6 +3,7 @@
 --
 -- http://hackage.haskell.org/packages/archive/amqp/0.3.3/doc/html/Network-AMQP.html
 -- http://videlalvaro.github.com/2010/09/haskell-and-rabbitmq.html
+-- http://www.haskell.org/haskellwiki/Implement_a_chat_server
 
 import System.IO
 import System.Environment
@@ -10,15 +11,12 @@ import Control.Monad
 import Text.ParserCombinators.Parsec
 import qualified Data.Map as M
 import Network.AMQP
+import Debug.Trace
 
-data CConf = CConf { hostname :: String
-                   , virtualHost :: String
-                   , login :: String
-                   , password :: String
-                   } deriving (Show)
-
+-- [[String]]
 cconf = many row
 
+-- [String]
 row = record >>= \result->
       eol >>
       return result
@@ -37,13 +35,31 @@ bare_string = many bare_char
 bare_char = noneOf "\n= "
 
 
-path = "test.conf"
+conf_name = "test.conf"
 
-main = readFile path >>= \c ->
-       case parse cconf "fail" c of
-            Left e -> putStr ("Error: " ++ path) >>
-                      print e
-            Right result -> mapM_ print result
+get_cconf :: String -> M.Map String String
+get_cconf s =
+    case parse cconf "get_cconf" s of
+         Left e -> trace ("Error: " ++ show e) $ M.empty
+         Right lst -> foldl addm M.empty lst
+    where addm m ss 
+              | 2 <= length ss = M.insert (ss !! 0) (ss !! 1) m
+              | otherwise = m
+ 
+connect :: M.Map String String -> IO Connection
+connect m = case validation of
+                 Nothing -> fail "Error: hostname/vhost/user/password?"
+                 Just (h, v, u, p) -> openConnection h v u p
+    where validation = M.lookup "hostname" m >>= \hostname ->  
+                       M.lookup "vhost" m >>= \vhost ->
+                       M.lookup "user" m >>= \user ->
+                       M.lookup "password" m >>= \password ->
+                       return (hostname, vhost, user, password)
+
+main = readFile conf_name >>= \c ->
+       let m = get_cconf c
+       in  connect m >>= \conn ->
+           closeConnection conn
 
 
 
