436b35a540f1fb8648d03779eaab379ea717b8e9
cfchou <root@wasabi.(none)>
2013-03-06 23:05:24 +0000

    server handles signals to quit gracefully

--- HRServer.hs
+++ HRServer.hs
@@ -1,6 +1,7 @@
 
 import System.IO
 import System.Posix.Signals
+import System.Exit ( exitSuccess )
 import Control.Concurrent.Chan
 import qualified Data.List as L
 import qualified Data.Set as S
@@ -13,11 +14,22 @@ conf_name = "test.conf"
 main =
     readFile conf_name >>= \c ->
     connect (get_cconf c) >>= \conn ->
+    installHandler sigTERM (Catch $ sigterm_handler "sigTERM" conn) Nothing >>
+    installHandler sigQUIT (Catch $ sigterm_handler "sigQUIT" conn) Nothing >>
+    installHandler sigINT (Catch $ sigterm_handler "sigINT" conn) Nothing >>
+
     openChannel conn >>= \chan ->
     declare_ctrl_server conn chan >>= \drct ->
     newChan >>= \fifo ->
     consumeMsgs chan (queueName $ dqopts drct) Ack (consume_msg fifo) >>
     produce_msg S.empty fifo drct
+
+
+sigterm_handler :: String -> Connection -> IO ()
+sigterm_handler reason conn =
+    (putStrLn $ "Exit for " ++ reason) >>
+    closeConnection conn >>
+    raiseSignal sigKILL
 
 -- Read FIFO, update the users list which is then converted to the format
 -- "user1:user2:user3" and finally published to client.
@@ -40,12 +52,11 @@ produce_msg users fifo drct =
 consume_msg :: Chan String -> (Message, Envelope) -> IO ()
 consume_msg fifo (m, e) = 
     let s = BL.unpack $ msgBody m
-    in  if length s > 2 && head s == ':' && (s !! 1) `elem` "+-" then
-            writeChan fifo (tail s) >>
-            ackEnv e
+    in  ackEnv e >>
+        if length s > 2 && head s == ':' && (s !! 1) `elem` "+-" then
+            writeChan fifo (tail s)
         else
-            ackEnv e
-
+            return ()
 
 
 
